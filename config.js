4f111898888211242be6fca813d69971fa0f8a21
Ashim Upadhaya <upadhayaashim@gmail.com>
2019-12-21 20:54:27 +0530

    Resolving heroku error of PORT

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongodb_url: process.env.mongodb_url || "mongodb://localhost:27017/urls",
-  port: process.env.port || 4000,
+  port: process.env.PORT || 4000,
   host: process.env.host || "http://localhost:4000/"
 };
